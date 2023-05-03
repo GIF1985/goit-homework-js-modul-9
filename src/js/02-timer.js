ab34c57166f0bc966c0c43dc44eec1ad11fb7b91
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-05-03 16:08:01 +0300

    Задание 2 - таймер обратного отсчета

---
+++ src/js/02-timer.js
@@ -0,0 +1,87 @@
+// Импортируем библиотеку flatpickr
+import flatpickr from 'flatpickr';
+
+// Добавляем импорт стилей для flatpickr
+import 'flatpickr/dist/flatpickr.min.css';
+
+// Находим элемент с id "datetime-picker" и кнопку с id "start-btn"
+const datetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('#start-btn');
+
+// Объявляем переменные для хранения выбранного времени и интервала времени
+let selectedMs;
+let timeInterval;
+
+// Опции для конфигурации flatpickr
+const options = {
+  enableTime: true, // Включаем выбор времени
+  time_24hr: true, // Используем 24-часовой формат времени
+  defaultDate: new Date(), // Устанавливаем текущую дату и время по умолчанию
+  minuteIncrement: 1, // Устанавливаем интервал выбора минут
+  onClose(selectedDates) {
+    // Обработчик события закрытия выбора времени
+    startBtn.disabled = true; // Отключаем кнопку старта
+    console.log(selectedDates[0]); // Выводим выбранную дату и время в консоль
+    selectedMs = Date.parse(selectedDates[0]); // Преобразуем выбранную дату и время в миллисекунды
+
+    // Проверяем, что выбранное время находится в будущем
+    if (selectedMs < new Date().getTime()) {
+      window.alert('Please choose a date in the future'); // Выводим сообщение об ошибке
+      return;
+    }
+
+    startBtn.disabled = false; // Включаем кнопку старта
+  },
+};
+
+// Добавляем обработчик события клика на кнопку старта
+startBtn.addEventListener('click', onStartBtnClick);
+
+// Функция обработки клика на кнопку старта
+function onStartBtnClick() {
+  //   console.log('click');
+  clearInterval(timeInterval); // Очищаем предыдущий интервал, чтобы не создавать конфликтов
+  timeInterval = setInterval(() => {
+    // Запускаем новый интервал
+    selectedMs -= 1000; // Вычитаем одну секунду из оставшегося времени
+    const diffMs = selectedMs - new Date().getTime(); // Вычисляем разницу во времени между выбранной датой и текущим временем
+    const diffObj = convertMs(diffMs); // Преобразуем разницу в объект с данными о времени
+    setDate(diffObj); // Устанавливаем отображение оставшегося времени на странице
+  }, 1000);
+}
+
+// Функция установки отображения оставшегося времени на странице
+function setDate(data) {
+  document.querySelector('[data-seconds]').innerText = data.seconds;
+  document.querySelector('[data-minutes]').innerText = data.minutes;
+  document.querySelector('[data-hours]').innerText = data.hours;
+  document.querySelector('[data-days]').innerText = data.days;
+}
+
+// Инициализация datetimePicker с опциями
+flatpickr(datetimePicker, options);
+
+// Функция для конвертации миллисекунд в дни, часы, минуты и секунды
+function convertMs(ms) {
+  // Количество миллисекунд в единице времени
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Оставшиеся дни
+  const days = Math.floor(ms / day);
+  // Оставшиеся часы
+  const hours = Math.floor((ms % day) / hour);
+  // Оставшиеся минуты
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Оставшиеся секунды
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// Примеры использования функции convertMs
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
